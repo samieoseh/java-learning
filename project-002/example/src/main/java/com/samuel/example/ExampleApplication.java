064591b5f6436566b9cf91902e7045630b11d9e5
samieIzone5 <samuel@izone5.com.ng>
2024-09-14 21:26:09 +0000

    Use @Component to create beans

--- project-002/example/src/main/java/com/samuel/example/ExampleApplication.java
+++ project-002/example/src/main/java/com/samuel/example/ExampleApplication.java
@@ -1,13 +1,40 @@
+//package com.samuel.example;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ExampleApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ExampleApplication.class, args);
+//	}
+//
+//}
+
 package com.samuel.example;
 
+import com.samuel.example.services.impl.ColourPrinterImpl;
+import com.samuel.example.services.ColourPrinter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class ExampleApplication implements CommandLineRunner {
+
+	private ColourPrinter colourPrinter;
+
+	public ExampleApplication(ColourPrinter colourPrinter) {
+		this.colourPrinter = colourPrinter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
-
+	@Override
+	public void run(final String... args) {
+		System.out.println(colourPrinter.print());
+	}
 }
+
